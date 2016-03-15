21ce1ba2672ead3f2c3de9140df9d977354431cd
SteveK <stevendeankennedy@gmail.com>
2016-03-15 12:36:53 -0400

    (#11) Added script for sin movement.

---
+++ Assets/Script/Effects/NumberFlier.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NumberFlier : MonoBehaviour {
+
+    [Tooltip("How fast to fly")]
+    public float speed;
+    [Tooltip("Distance of wave movement")]
+    public float amplitude;
+    [Tooltip("Wave width")]
+    public float frequency;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb.velocity = new Vector3(0, 0, speed);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += amplitude * (Mathf.Sin(2 * Mathf.PI * frequency * Time.time) - Mathf.Sin(2 * Mathf.PI * frequency * (Time.time - Time.deltaTime))) * transform.forward;
+	}
+}

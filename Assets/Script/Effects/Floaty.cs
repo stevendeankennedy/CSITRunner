ff892902247e75b0b6764a86d11ea7e6e364b794
SteveK <stevendeankennedy@gmail.com>
2016-03-15 13:14:27 -0400

    (#11)  * Added Floaties.  A Floaty is an object that can move and spin around in place, for whatever purpose.  * Added a FloatyFactory script with nothing in it.

---
+++ Assets/Script/Effects/Floaty.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+//using System.Collections;
+
+/**
+    A Floaty moves around eratically based on a fixed coordinate of its parent object.
+    Usage: put a bunch of these in an Empty GameObject and give them torque.
+**/
+public class Floaty : MonoBehaviour {
+
+    [Tooltip("How to rotate")]
+    public Vector3 torque;
+    [Tooltip("Distance of wave movement")]
+    public float amplitude;
+    [Tooltip("Wave width")]
+    public float frequency;
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition += amplitude * (Mathf.Sin(2 * Mathf.PI * frequency * Time.time) - Mathf.Sin(2 * Mathf.PI * frequency * (Time.time - Time.deltaTime))) * transform.up;
+        Vector3 cRot = transform.rotation.eulerAngles;
+        cRot += torque;
+        transform.localRotation = Quaternion.Euler(cRot);
+    }
+}
---
+++ Assets/Script/Effects/FloatyFactory.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+//using System.Collections;
+
+
+// (c)2016 Steven Dean Kennedy
+public class FloatyFactory : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}

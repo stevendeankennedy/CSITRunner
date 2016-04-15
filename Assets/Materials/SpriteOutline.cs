e5af3cf10671302908a651a7a4396b6400ade90a
SteveK <stevendeankennedy@gmail.com>
2016-04-15 09:46:37 -0400

    (#25) Added shader for drawing sprite outlines.  (from https://www.reddit.com/r/Unity2D/comments/4eafla/sprite_outlines_in_unity_using_shaders/).
    Created extra versions of characters with outlines already drawn for comparisons.

---
+++ Assets/Materials/SpriteOutline.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+//using System.Collections;
+
+[ExecuteInEditMode]
+public class SpriteOutline : MonoBehaviour {
+    public Color color = Color.white;
+
+    private SpriteRenderer spriteRenderer;
+    MaterialPropertyBlock mpb;
+
+    void Awake()
+    {
+        mpb = new MaterialPropertyBlock();
+    }
+
+    void OnEnable()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        UpdateOutline(true);
+    }
+    
+    void OnDisable()
+    {
+        UpdateOutline(false);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        UpdateOutline(true);
+	}
+
+    void UpdateOutline(bool outline)
+    {
+        //MaterialPropertyBlock mpb = new MaterialPropertyBlock();
+        spriteRenderer.GetPropertyBlock(mpb);
+        mpb.SetFloat("_Outline", outline ? 1f : 0);
+        mpb.SetColor("_OutlineColor", color);
+        spriteRenderer.SetPropertyBlock(mpb);
+    }
+}
